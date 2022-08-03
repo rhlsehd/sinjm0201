69572a5aea8c92d33af483069dce3e5b216440d0
rhlsehd <104844225+rhlsehd@users.noreply.github.com>
2022-08-03 20:12:22 +0900

    incomplete
    incomplete

---
+++ accountbook.c
@@ -0,0 +1,69 @@
+#pragma warning(disable:4996) //c4996 오류 방지
+#define _TM_ // struct 재정의 오류 방지
+#include <stdio.h>
+#include <time.h> //localtime 헤더파일
+#include <windows.h> //system(cls) 헤더파일
+
+#if !defined(_TM_) //localtime 구조체
+struct tm
+{
+	int tm_mday;
+	int tm_mon;
+	int tm_year;
+};
+#endif
+
+int total = 50000;
+
+void manage();
+void modify();
+int main()
+{
+	int sel;
+	printf("1 : 관리\n2 : 수정\n");
+	scanf_s("%d", &sel);
+	system("cls");
+
+	switch (sel)
+	{
+	case 1:
+		manage();
+		break;
+	case 2:
+		modify();
+		break;
+	}
+	system("pause>null"); 
+	return 0;
+}
+
+void manage()
+{
+	FILE* fp = fopen("accountbook.txt", "w");
+	int income, expense;
+	time_t timer;
+	struct tm* t;
+	timer = time(NULL);
+	t = localtime(&timer);
+
+	printf("현재날짜 : %d년 %d월 %d일 \n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
+	printf("수입 : ");
+	scanf_s("%d", &income);
+	printf("지출 : ");
+	scanf_s("%d", &expense);
+	printf("잔액 : %d\n", total - (expense - income));
+
+	int *result = total - (expense - income);
+	
+	fprintf(fp, "%d년 %d월 %d일 \n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
+	fprintf(fp, "수입 : %d\n", income);
+	fprintf(fp, "지출 : %d\n", expense);
+	fprintf(fp, "잔액 : %d\n", result);
+	fprintf(fp, "----------------\n");
+	fclose(fp);
+}
+
+void modify()
+{
+	printf("asd");
+}
