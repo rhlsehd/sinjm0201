958eb4b0ed2fb00c1de0d2288ff88d679663cba1
rhlsehd <104844225+rhlsehd@users.noreply.github.com>
2022-08-09 03:12:47 +0900

    Add files via upload

---
+++ foundation prt2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	int num;
+
+	while (1)
+	{
+
+		std::cout << "판매 금액을 만원 단위로 입력 (-1 to end) : ";
+		std::cin >> num;
+		if (num == -1)
+		{
+			std::cout << "프로그램을 종료합니다." << std::endl;
+			break;
+		}
+
+		else
+			std::cout << "이번 달 급여 : " << 50 + num * 0.12 << "만원" << std::endl;
+	}
+
+	return 0;
+}
